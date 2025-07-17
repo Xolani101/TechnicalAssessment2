349313e9354bc62b2aba3df2bde3a689fdbc42b1
Xolani Maphumulo <mrxolanizn@gmail.com>
2025-07-17 23:20:53 +0200

    Fixed error.

--- tests/ui/uiautomationtesting.spec.js
+++ tests/ui/uiautomationtesting.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const { getTestData } = require('./utils/utils');
+const { getTestData } = require('../utils/utils');
 
 const testData = getTestData();
 
